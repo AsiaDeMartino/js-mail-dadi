5da24bb5bf303e754ac0d5d57ebd81c289b960e7
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-09 21:45:19 +0100

    aggiunti file e creata struttura base js

---
+++ myscript.js
@@ -0,0 +1,10 @@
+//GIOCO DEI DADI
+    //genero un umero random per il computer
+    //genero un numero random per l'utente
+    //confronto i due numeri --> alert che dice chi ha vinto 
+
+
+//MAIL
+    //chiedo all'utente la sua mail 
+    //creo l'array con tutte le mail 
+    //creo un ciclo for per controllare se la mail inserita dall'utente è presente nell'array 
