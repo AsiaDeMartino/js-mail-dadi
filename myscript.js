1211997414ca45cd814492e3c357a81c8d9d5b58
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-09 22:35:04 +0100

    aggiunte condizioni/ inserito in .addEventListener

--- myscript.js
+++ myscript.js
@@ -1,13 +1,23 @@
 //GIOCO DEI DADI
-//genero un numero random per il computer
+const dadi = document.getElementById("dadi")
+dadi.addEventListener("click",function() {
+    //genero un numero random per il computer
 let numeroComputer = Math.floor(Math.random()* 6) +1
 console.log(numeroComputer)
 
-//genero un numero random per l'utente
+    //genero un numero random per l'utente
 let numeroUtente = Math.floor(Math.random()* 6) +1
 console.log(numeroUtente)
 
-//confronto i due numeri --> alert che dice chi ha vinto 
+    //confronto i due numeri --> alert che dice chi ha vinto 
+if (numeroComputer > numeroUtente) {
+    alert("Mi dispiace, hai perso!")
+  } else if (numeroComputer < numeroUtente){
+    alert("Complimenti, hai vinto!")
+ } else {
+    alert("Pareggio...")
+ }
+})
 
 
 
