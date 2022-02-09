0c29a1335eacd484ddfd66550b2a09382cad300f
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-09 21:56:45 +0100

    gioco dei dadi-create variabili numeri random

--- myscript.js
+++ myscript.js
@@ -1,7 +1,14 @@
 //GIOCO DEI DADI
-    //genero un umero random per il computer
-    //genero un numero random per l'utente
-    //confronto i due numeri --> alert che dice chi ha vinto 
+//genero un numero random per il computer
+let numeroComputer = Math.floor(Math.random()* 6) +1
+console.log(numeroComputer)
+
+//genero un numero random per l'utente
+let numeroUtente = Math.floor(Math.random()* 6) +1
+console.log(numeroUtente)
+
+//confronto i due numeri --> alert che dice chi ha vinto 
+
 
 
 //MAIL
