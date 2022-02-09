fd07cfe50a53f549183866c8821d8e94376ba143
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-09 23:09:09 +0100

    mail-creato array e html

--- myscript.js
+++ myscript.js
@@ -1,27 +1,36 @@
 //GIOCO DEI DADI
-const dadi = document.getElementById("dadi")
+const dadi = document.getElementById("dadi");
 dadi.addEventListener("click",function() {
     //genero un numero random per il computer
-let numeroComputer = Math.floor(Math.random()* 6) +1
-console.log(numeroComputer)
+let numeroComputer = Math.floor(Math.random()* 6) +1;
+console.log(numeroComputer);
 
     //genero un numero random per l'utente
-let numeroUtente = Math.floor(Math.random()* 6) +1
-console.log(numeroUtente)
+let numeroUtente = Math.floor(Math.random()* 6) +1;
+console.log(numeroUtente);
 
     //confronto i due numeri --> alert che dice chi ha vinto 
 if (numeroComputer > numeroUtente) {
-    alert("Mi dispiace, hai perso!")
+    alert("Mi dispiace, hai perso!");
   } else if (numeroComputer < numeroUtente){
-    alert("Complimenti, hai vinto!")
+    alert("Complimenti, hai vinto!");
  } else {
-    alert("Pareggio...")
+    alert("Pareggio...");
  }
 })
 
 
+//MAIL 
+    //creo l'array con tutte le mail
+const database = [
+    "UC3xtw6T0NNOAsp@gmail.com",
+    "pWvzIP2ig94mbJ@gmail.com",
+    "nb3GZCyS6GyRnBc@gmail.com",
+    "bBI36RHtC83OyBA@gmail.com",
+    "lKwmu2usrYtwZr@gmail.com",
+    "example@gmail.com"
+];
 
-//MAIL
-    //chiedo all'utente la sua mail 
-    //creo l'array con tutte le mail 
+
+    
     //creo un ciclo for per controllare se la mail inserita dall'utente è presente nell'array 
